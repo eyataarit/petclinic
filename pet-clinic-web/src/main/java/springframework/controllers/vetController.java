eb5122f2c6a589bcb1438d8c44184be72c4f09b2
Aya Ben Taarit <taarit.aya@gmail.com>
2022-05-30 14:16:35 +0200

    Add vet clinic Index and controller. close #6

---
+++ pet-clinic-web/src/main/java/springframework/controllers/vetController.java
@@ -0,0 +1,12 @@
+package springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class vetController {
+    @RequestMapping({"/vets"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
