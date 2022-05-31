2a715f7a36f6059470c3bf33ae8f24c9f436fc98
Aya Ben Taarit <taarit.aya@gmail.com>
2022-05-31 14:43:47 +0200

    list vets on index vets page. close #14

--- pet-clinic-web/src/main/java/springframework/controllers/vetController.java
+++ pet-clinic-web/src/main/java/springframework/controllers/vetController.java
@@ -1,12 +1,21 @@
 package springframework.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springframework.services.VetService;
 
 @Controller
 public class vetController {
+    private final VetService vetService;
+
+    public vetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
