83084511d605a76938b0a00496daada990dc5b49
Aya Ben Taarit <taarit.aya@gmail.com>
2022-06-06 21:01:18 +0200

    add visit entity close #27

--- pet-clinic-data/src/main/java/springframework/model/Pet.java
+++ pet-clinic-data/src/main/java/springframework/model/Pet.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -26,6 +28,10 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL , mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
 
 
 }
--- pet-clinic-data/src/main/java/springframework/model/Visit.java
+++ pet-clinic-data/src/main/java/springframework/model/Visit.java
@@ -1,5 +1,21 @@
 package springframework.model;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
 
 }
