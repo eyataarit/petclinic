6e6cc10a7b8d5faeef3f9c17ea7974715f2d121d
Aya Ben Taarit <taarit.aya@gmail.com>
2022-06-07 14:43:27 +0200

    add spring data jpa service for specialty, close #33

---
+++ pet-clinic-data/src/main/java/springframework/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package springframework.services.map;
+
+import org.springframework.stereotype.Service;
+import springframework.model.Specialty;
+import springframework.services.SpecialtyService;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty,Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
